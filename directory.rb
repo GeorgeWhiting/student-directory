ee788b840ac49efee2a7576c51b1865158c89077
George Whiting <george_whiting@hotmail.com>
2017-11-07 15:37:45 +0000

    Added exercise 6: Centered output

--- directory.rb
+++ directory.rb
@@ -8,27 +8,31 @@ def input_students
   puts "Please enter the nationality of the student"
   nationality = gets.chomp
   while true do
-    students << {name: name, cohort: :november, height: height, nationality: nationality}
+    students << {name: name, cohort: :November, height: height, nationality: nationality}
     puts "Now we have #{students.count} students"
+    puts "Please enter the name of a student"
     name = gets.chomp
     if name.empty?
       break
     end
+    puts "Please enter the height of the student"
     height = gets.chomp
+    puts "Please enter the nationality of the student"
     nationality = gets.chomp
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 def print(names)
-  names.each_with_index{|name,index| puts "#{index+1}. #{name[:name]} (#{name[:cohort]} cohort). Height: #{name[:height]}, Nationality: #{name[:nationality]}"}
+  names.each_with_index{|name,index| puts "#{index+1}. #{name[:name]} (#{name[:cohort]} cohort). Height: #{name[:height]}, Nationality: #{name[:nationality]}".center(100)}
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+  puts
+  puts "Overall, we have #{names.count} great students.".center(100)
 end
 #User inputs students in to array
 students = input_students
