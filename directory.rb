f072f08978632b31efb23f76bb468e59520461ef
George Whiting <george_whiting@hotmail.com>
2017-11-07 15:32:51 +0000

    Added exercise 5: added height and nationality properties for students

--- directory.rb
+++ directory.rb
@@ -1,12 +1,21 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of a student"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
+  puts "Please enter the height of the student"
+  height = gets.chomp
+  puts "Please enter the nationality of the student"
+  nationality = gets.chomp
+  while true do
+    students << {name: name, cohort: :november, height: height, nationality: nationality}
     puts "Now we have #{students.count} students"
     name = gets.chomp
+    if name.empty?
+      break
+    end
+    height = gets.chomp
+    nationality = gets.chomp
   end
   students
 end
@@ -16,11 +25,7 @@ def print_header
   puts "-------------"
 end
 def print(names)
-  i=0
-  until i == names.length
-    puts "#{i+1}. #{names[i][:name]} (#{names[i][:cohort]} cohort)"
-    i+=1
-  end
+  names.each_with_index{|name,index| puts "#{index+1}. #{name[:name]} (#{name[:cohort]} cohort). Height: #{name[:height]}, Nationality: #{name[:nationality]}"}
 end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students."
