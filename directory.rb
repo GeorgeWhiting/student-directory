3f82b713e60f4316a043150b19f35be291650588
George Whiting <george_whiting@hotmail.com>
2017-11-08 22:38:38 +0000

    Added messages when menu options are ran

--- directory.rb
+++ directory.rb
@@ -17,6 +17,7 @@ def input_students
     message = "Now we have #{@students.count} students"
     puts @students.count == 1? message.chop : message
   end
+  puts "Students added successfully"
 end
 
 def print_header
@@ -25,7 +26,6 @@ def print_header
 end
 
 def print_students_list
-
   @students.sort_by!{|students| @months.index(students[:cohort])}
   @students.group_by{|students| students[:cohort]}.each do |students|
     students[1].each {|name| puts "#{name[:name]} (#{name[:cohort]} cohort). Height: #{name[:height]}, Nationality: #{name[:nationality]}".center(100)}
@@ -47,6 +47,7 @@ def interactive_menu
 end
 
 def print_menu
+  puts
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save students to students.csv"
@@ -77,15 +78,15 @@ def menu_switch(selection)
   end
 end
 
-def save_students
-  file = File.open("students.csv","w")
+def save_students(filename = "students.csv")
+  file = File.open(filename,"w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort], student[:height], student[:nationality]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
-  puts "Students saved to file"
+  puts "Students saved to #{filename}"
 end
 
 def load_students(filename = "students.csv")
@@ -95,16 +96,15 @@ def load_students(filename = "students.csv")
     fill_student_array(name, cohort, height, nationality)
   end
   file.close
+  puts "Loaded #{@students.count} students from #{filename}"
 end
 
 def try_load_students
   filename = ARGV.first
   if filename.nil?
     load_students
-    puts "Loaded #{@students.count} students from students.csv"
   elsif File.exists?(filename)
     load_students(filename)
-    puts "Loaded #{@students.count} students from #{filename}"
   else
     puts "Sorry, #{filename} doesn't exist"
     exit
