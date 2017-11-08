9fa95e55483925aee585273bf953cd294ab559a8
George Whiting <george_whiting@hotmail.com>
2017-11-08 14:09:33 +0000

    Refactored menu and students variable

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
+@students = []
 def input_students
-  students = []
   $months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December, "N/A".to_sym]
   while true do
     puts "Please enter the name of a student. To finish, just hit return."
@@ -26,60 +26,61 @@ def input_students
     if nationality.empty?
       nationality = "N/A"
     end
-    students << {name: name, cohort: cohort, height: height, nationality: nationality}
-    message = "Now we have #{students.count} students"
-    puts students.count == 1? message.chop : message
+    @students << {name: name, cohort: cohort, height: height, nationality: nationality}
+    message = "Now we have #{@students.count} students"
+    puts @students.count == 1? message.chop : message
   end
-  students
 end
 
 def print_header
   puts "The students of Villains Academy".center(100)
   puts "-------------".center(100)
 end
-def print(names)
-  names.sort_by!{|a| $months.index(a[:cohort])}
-  names.group_by{|d| d[:cohort]}.each do |g|
+
+def print_students_list
+  @students.sort_by!{|a| $months.index(a[:cohort])}
+  @students.group_by{|d| d[:cohort]}.each do |g|
     g[1].each_with_index{|name,index| puts "#{index+1}. #{name[:name]} (#{name[:cohort]} cohort). Height: #{name[:height]}, Nationality: #{name[:nationality]}".center(100)}
   end
 end
-def print_footer(names)
-  message = "Overall, we have #{names.count} great students"
+
+def print_footer
+  message = "Overall, we have #{@students.count} great students"
   puts
-  puts names.count == 1? message.chop.center(100) : message.center(100)
+  puts @students.count == 1? message.chop.center(100) : message.center(100)
   puts
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header unless students.count == 0
-        print(students) unless students.count == 0
-        print_footer(students) unless students.count == 0
-      when "9"
-        exit
-      else
-        puts "Thats not a command"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header unless @students.count == 0
+  print_students_list unless @students.count == 0
+  print_footer unless @students.count == 0
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Thats not a command"
+  end
+end
+
 interactive_menu
-=begin
-#User inputs students in to array
-students = input_students
-#Then print them all
-print_header unless students.count == 0
-print(students) unless students.count == 0
-#Prints total number of students
-print_footer(students) unless students.count == 0
-=end
